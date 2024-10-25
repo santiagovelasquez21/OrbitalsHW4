0185b83c63c0c935fd20b560416db8e6e8e66213
S. Velasquez <velas137@umn.edu>
2024-10-25 09:59:30 -0500

    Initialize variables

--- OrbitalMechHW4.m
+++ OrbitalMechHW4.m
@@ -1,3 +1,32 @@
-%% Obtaining input r, r_0
-r_0 = [];
-r = [];
+%% Obtaining initial variables
+R0 = [0.5 0.6 0.7];
+R1 = [0 1 0];
+dt = 0.9667663;
+mu = 1;
+dir = 2; %1: Short way, 2: Long way
+z0 = dt;
+
+
+[V1,V2] = lambert(mu,R0,R1,dt,z0,dir); % 
+
+
+
+
+%% Error
+relerr = 1;
+tol = 1e-5;
+xi = 4;
+n = 0;
+nmax = 20;
+
+while relerr>tol
+    xi1=xi-f(xi)/g(xi); %get new x
+    relerr=abs((xi1-xi)/xi); %relative error
+    xi=xi1;
+    n=n+1;
+if n>nmax
+break;
+end
+end
+Given 𝑓𝑓 𝑥𝑥 , 𝑓𝑓 ′ 𝑥𝑥 = 𝑔𝑔(𝑥𝑥)
+
